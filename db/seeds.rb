0318aee31c0f8ddb7efb7da7d9465fead220edf8
Theo <theo.gianella@proton.me>
2023-03-13 17:19:30 +0100

    Remade seed : more objects with more attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,25 +4,28 @@ Order.destroy_all
 OrderItem.destroy_all
 Category.destroy_all
 
-3.times do |_|
+10.times do |_|
   User.create!(email: Faker::Internet.email,
                password: 'foobar')
 end
 
-3.times do |_|
+20.times do |_|
   Order.create!(user: User.all.sample)
 end
 
-3.times do |_|
+5.times do |_|
   Category.create(name: Faker::Hobby.activity)
 end
 
-3.times do |_|
+20.times do |_|
   Item.create!(title: Faker::Games::Pokemon.name,
                category: Category.all.sample)
 end
 
 Order.all.each do |order|
-  OrderItem.create!(order: order,
-                    item: Item.all.sample)
+  Item.all.sample(rand(5)).each do |item|
+    OrderItem.create!(order: order,
+                      item: item,
+                      quantity: rand(10))
+  end
 end
