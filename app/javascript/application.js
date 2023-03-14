22d785c944af5481da66068b76f3df906e84d71c
steakinzer <clementjodin1u@gmail.com>
2023-03-14 10:35:56 +0100

    first model of navbar added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,37 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+let dropprofil = document.querySelector("#user-menu-button")
+let dropprofilmenu = document.querySelector(".dropp")
+
+
+console.log(dropprofil)
+console.log(dropprofilmenu)
+dropprofilmenu.style.display = "none"
+dropprofil.addEventListener("click", e => {
+  if (dropprofilmenu.style.display === "none"){
+    dropprofilmenu.style.display = "block"
+  }
+  else {
+    dropprofilmenu.style.display = "none"
+  }
+})
+
+
+
+let burgermenu = document.querySelector(".burger")
+let dropburgermenu = document.querySelector(".tuto")
+dropburgermenu.style.display ="none"
+burgermenu.addEventListener("click", e =>{
+  // dropburgermenu.classList.toggle("block")
+  if (dropburgermenu.style.display ==="none"){
+    dropburgermenu.style.display = "block"
+  }
+  else {
+    dropburgermenu.style.display = "none"
+  }
+})
+console.log(burgermenu)
+console.log(dropburgermenu)
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'items/index'
   root 'items#index'
   devise_for :users
   resources :items
   resources :orders
+  get '/contact' => "static_pages#contact"
+  get '/about' => "static_pages#about"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
