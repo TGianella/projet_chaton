b59d56db521eff687a32099f60a409cc1b588f06
Theo <theo.gianella@proton.me>
2023-03-15 10:13:16 +0100

    Created model and migration cart

--- db/migrate/20230313131657_create_order_items.rb
+++ db/migrate/20230313131657_create_order_items.rb
@@ -1,8 +1,9 @@
 class CreateOrderItems < ActiveRecord::Migration[7.0]
   def change
     create_table :order_items do |t|
-      t.integer :quantity
-      t.belongs_to :order, index: true
+      t.integer :quantity, default: 1
+      t.integer :order_id
+      t.integer :cart_id
       t.belongs_to :item, index: true
 
       t.timestamps
