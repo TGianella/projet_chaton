6c8ffb914f62b24a1314392b5f85b80813c4008a
Theo <theo.gianella@proton.me>
2023-03-15 13:46:49 +0100

    Empty cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,4 +2,13 @@ class CartsController < ApplicationController
   def show
     @cart = Cart.find(params[:id])
   end
+
+  def destroy
+    @cart = current_user.cart
+    @cart.destroy
+    @cart = Cart.new
+    @cart.user = current_user
+    @cart.save
+    redirect_to cart_path(@cart)
+  end
 end
