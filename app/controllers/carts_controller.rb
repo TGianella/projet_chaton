25fa149ea20059c2515653bb1d76fd47f336b0f2
Thomas <thomas.kanthack@gmail.com>
2023-03-15 18:08:20 +0100

    front of the cart page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,6 +3,9 @@ class CartsController < ApplicationController
 
   def show
     @cart = Cart.find(params[:id])
+    @item_quantity = item_quantity
+    @item_quantity_price = item_quantity_price
+
   end
 
   def destroy
@@ -14,6 +17,7 @@ class CartsController < ApplicationController
     redirect_to cart_path(@cart)
   end
 
+
   private
 
   def my_cart?
@@ -21,4 +25,22 @@ class CartsController < ApplicationController
 
     redirect_back fallback_location: root_path
   end
+
+  def item_quantity
+    @amount = 0
+    @cart.order_items.each do |order_item|
+      @amount += order_item.quantity
+    end
+    @amount
+  end
+
+  def item_quantity_price
+    @total = 0
+    @cart.order_items.each do |order_item|
+      @total += order_item.item.price * order_item.quantity
+    end
+    '%.02f' % @total
+  end
+
+
 end
