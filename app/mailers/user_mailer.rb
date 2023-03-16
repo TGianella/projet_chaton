0c175286248bb192a699b696762d5335cac75cda
rockethelll <rockethell@hotmail.com>
2023-03-16 12:13:03 +0100

    methods mailer done

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,24 +8,21 @@ class UserMailer < ApplicationMailer
   end
 
   def send_order
-    @user = current_user
     @order = Order.last
-    @order_items = OrderItem.where(order_id: @order.id)
-    @total_price = 0
-    @order_items.each do |order_item|
-      @total_price += order_item.quantity * order_item.item.price
+    if @order.status == 'paid'
+      @user = @order.user.email
+      @order_items = order.order_items
+      mail(to: @user.email, subject: 'Merci pour ta commande')
     end
-    mail(to: @user.email, subject: 'Merci pour ta commande')
   end
 
   def order_recap_to_admin
     @user = User.where(admin: true).pluck(:email)
     @order = Order.last
-    @order_items = OrderItem.where(order_id: @order.id)
-    @total_price = 0
-    @order_items.each do |order_item|
-      @total_price += order_item.quantity * order_item.item.price
+    if @order.status == 'paid'
+      @user = @order.user.email
+      @order_items = order.order_items
+      mail(to: @user.email, subject: 'Nouvelle commande')
     end
-    mail(to: @user.email, subject: 'Nouvelle commande')
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,16 @@ class Order < ApplicationRecord
   validates :status, presence: true,
                      inclusion: { in: %w[pending paid],
                                   message: '%<value>s is not a valid status' }
+
+  def total_price
+    sum = 0
+    order_items.each { |order_item| sum += order_item.total_price }
+    sum
+  end
+
+  def total_quantity
+    sum = 0
+    order_items.each { |order_item| sum += order_item.quantity }
+    sum
+  end
 end
