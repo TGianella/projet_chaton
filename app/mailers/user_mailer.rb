b2508bfc58239d5ef11fb9e3f42c53cf91ba23ec
rockethelll <rockethell@hotmail.com>
2023-03-16 11:13:15 +0100

    add user admin column

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,26 @@ class UserMailer < ApplicationMailer
     @url = 'https://awesomecatpics-development.herokuapp.com/'
     mail(to: @user.email, subject: 'Bienvenue sur AwesomeCatPics !')
   end
+
+  def send_order
+    @user = current_user
+    @order = Order.last
+    @order_items = OrderItem.where(order_id: @order.id)
+    @total_price = 0
+    @order_items.each do |order_item|
+      @total_price += order_item.quantity * order_item.item.price
+    end
+    mail(to: @user.email, subject: 'Merci pour ta commande')
+  end
+
+  def order_recap_to_admin
+    @user = User.where(admin: true).pluck(:email)
+    @order = Order.last
+    @order_items = OrderItem.where(order_id: @order.id)
+    @total_price = 0
+    @order_items.each do |order_item|
+      @total_price += order_item.quantity * order_item.item.price
+    end
+    mail(to: @user.email, subject: 'Nouvelle commande')
+  end
 end
